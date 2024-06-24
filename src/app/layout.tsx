9242f0717f5cc7eb5d5d825f293a23a44694b346
Rami Valta <rami.valta@gmail.com>
2024-06-24 14:46:39 +0300

    Try to fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,11 @@ export default async function RootLayout({
     <html lang="en">
       <head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
       </head>
       <body>
