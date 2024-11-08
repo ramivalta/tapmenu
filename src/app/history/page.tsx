5439e2b40d8f846161b3859c69c669c2df7e688e
Rami Valta <rami.valta@bonsky.com>
2024-11-08 13:04:30 +0200

    Fix build

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,30 +1,31 @@
+import { Box } from "@chakra-ui/react";
 import BrewHistory from "./BrewHistory";
 
 const token = process.env.BREWFATHER_API_TOKEN;
 
 export default async function History() {
-  const batches = await fetch(
-    // "https://api.brewfather.app/v2/batches?include=[brewday,recipe,batchNotes,batch.notes]&status=Completed",
-    "https://api.brewfather.app/v2/batches?complete=True&status=Archived&limit=50",
-    // "https://api.brewfather.app/v2/batches?complete=True",
-    {
-      headers: {
-        Authorization: `Basic ${token}`,
-      },
-      next: {
-        revalidate: 1,
-      },
+  try {
+
+    const batches = await fetch(
+      // "https://api.brewfather.app/v2/batches?include=[brewday,recipe,batchNotes,batch.notes]&status=Completed",
+      // "https://api.brewfather.app/v2/batches?complete=True",
+      "https://api.brewfather.app/v2/batches?complete=True&status=Archived&limit=50",
+      {
+        headers: {
+          Authorization: `Basic ${token}`,
+        },
+        next: {
+          revalidate: 1,
+        },
+      }
+    );
+    const batchesData = await batches.json();
+  
+    if (batchesData) {
+      return <BrewHistory batches={batchesData} />;
     }
-  );
-  const batchesData = await batches.json();
-
-  //   console.log("HISTORY", batchesData);
-
-  if (batchesData) {
-    return <BrewHistory batches={batchesData} />;
+  } catch (e: any) {
+    console.error(e);
+    return <Box>Error fetching data</Box>;
   }
-
-  //   console.log(batchesData);
 }
-
-// bGRVYU1nbXh2TmJFTDVrZFBUSEFrM0VUSUVmMTpsczRwdFJWY1Q1TVFBR1hIaGxLWmZjQVVxUlU4YU1LUUthR01DM0JrQ3BHUndrOUZxdEpiOUVJU0VkYzZPSGJ3
