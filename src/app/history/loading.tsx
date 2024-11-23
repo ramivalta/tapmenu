1cd1e693ecb7a0309dc05ac633c7f017e8e093aa
Rami Valta <rami.valta@bonsky.com>
2024-11-23 13:00:29 +0200

    Put loading indicators and give default value to label select dropdown

---
+++ src/app/history/loading.tsx
@@ -0,0 +1,12 @@
+import { Spinner, Center } from "@chakra-ui/react";
+
+const Loading = () => {
+  return (
+    <Center mt="120px">
+      <Spinner />
+    </Center>
+  );
+};
+
+
+export default Loading;
--- src/app/label/SelectLabel.tsx
+++ src/app/label/SelectLabel.tsx
@@ -1,25 +1,37 @@
 "use client";
 
+import { Spinner, Flex } from "@chakra-ui/react";
 import { orderBy } from "lodash";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Fragment, useTransition } from "react";
 
 const SelectEtiquette = ({ batches }: { batches: any[] }) => {
+  const [isPending, startTransition] = useTransition();
+  const searchParams = useSearchParams();
   const router = useRouter();
   return (
-    <select
-      onChange={(e) => {
-        router.push(`/label?beer=${e.target.value}`);
-      }}
-    >
-      <option>-</option>
-      {orderBy(batches, "brewDate", "desc").map((batch: any, index: number) => {
-        return (
-          <option value={batch._id} key={batch.id + "_" + index}>
-            {batch?.recipe?.name}
-          </option>
-        );
-      })}
-    </select>
+    <Flex alignItems="center" gap="2">
+      <select
+        defaultValue={searchParams.get("beer") ?? undefined}
+        onChange={(e) => {
+          startTransition(() => {
+            router.push(`/label?beer=${e.target.value}`);
+          });
+        }}
+      >
+        <option>-</option>
+        {orderBy(batches, "brewDate", "desc").map(
+          (batch: any, index: number) => {
+            return (
+              <option value={batch._id} key={batch.id + "_" + index}>
+                {batch?.recipe?.name}
+              </option>
+            );
+          }
+        )}
+      </select>
+      {isPending ? <Spinner size="xs" /> : null}
+    </Flex>
   );
 };
 
